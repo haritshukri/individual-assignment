823b2ca1208c703b4620888285bc418c5490b51f
haritshukri <mharitshukri@gmail.com>
2022-11-08 15:09:34 +0000

    1 commit

---
+++ fork.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[]){
+	int id = fork();
+	printf("Hello World from id: %d\n", id);
+	return 0;
+}
